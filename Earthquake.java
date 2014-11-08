91405d0ccf4554f0bc3eaea35c3e0a4cfe8a673b
Andrew Arnopoulos <andrewarnopoulos@gmail.com>
2014-11-08 14:42:38 -0700

    Fixed problems

--- Earthquake.java
+++ Earthquake.java
@@ -3,26 +3,24 @@ import java.util.Random;
 import com.google.appengine.labs.repackaged.org.json.JSONException;
 import com.google.appengine.labs.repackaged.org.json.JSONObject;
 
-//import Data;
-
 public class Earthquake extends Data {
 	public static Earthquake[] earthquakes() {
 		Earthquake[] earthquakes = new Earthquake[10];
 		for (int i = 0; i < earthquakes.length; i++) {
 			Random r = new Random();
-			earthquakes[i] = Earthquake.earthquake[r.nextInt(Earthquake.earthquake.length)];
+			earthquakes[i] = Data.earthquake[r.nextInt(Data.earthquake.length)];
 		}
 		return earthquakes;
 	}
-
+	
 	double x,y,magnitude;
-
+	
 	public Earthquake(double x, double y, double magnitude) {
 		this.x = x;
 		this.y = y;
 		this.magnitude = magnitude;
 	}
-
+	
 	public JSONObject toJson() {
 		JSONObject json = new JSONObject();
 		JSONObject coordinates = new JSONObject();
@@ -32,7 +30,7 @@ public class Earthquake extends Data {
 			json.put("coordinates",coordinates);
 			json.put("magnitude",magnitude);
 		} catch (JSONException e) {
-
+			
 		}
 		return json;
 	}
